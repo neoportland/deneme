7cccf2f4617b00efc7c032544ee8e25e6a110017
nevzat kıyak <nevzatkyak3@gmail.com>
2024-05-02 15:16:00 +0300

    componnetler klasörü oluştu

---
+++ comenents/AlbumList.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AlbumList = () => {
+  return (
+    <div>
+      <h2>bu bölümde albümeri bir alt kompenenete gönderebilir miyim </h2>
+    </div>
+  );
+};
+
+export default AlbumList;
