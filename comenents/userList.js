a01104c8a09bb953d25aaa775158758ffd5fa173
nevzat kıyak <nevzatkyak3@gmail.com>
2024-05-02 15:18:21 +0300

    component altına user eklendi

---
+++ comenents/userList.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const userList = () => {
+  return (
+    <div>hello can you hear me this is ı dont know what a fuck benefits</div>
+  );
+};
+
+export default userList;
