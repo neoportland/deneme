c873b941285e7d35376e0f64cf4e24d1472e4e5e
nevzat kıyak <nevzatkyak3@gmail.com>
2024-05-02 15:26:16 +0300

    userlist güncelleme

--- comenents/userList.js
+++ comenents/userList.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const userList = () => {
   return (
-    <div>hello can you hear me this is ı dont know what a fuck benefits</div>
+    <div>
+      hello can you hear me this is ı dont know what a fuck benefits
+      <p>sen gidince bak neler oldu kalbimin ucu yandı tutuştu </p>
+    </div>
   );
 };
 
